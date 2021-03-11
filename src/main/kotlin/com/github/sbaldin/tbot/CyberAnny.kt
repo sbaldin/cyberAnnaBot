55e3737593095d5b0b08a4d33730f051638b228f
Sergey Baldin <sbaldin@riskmatch.com>
2021-03-11 14:48:57 +0700

    Bot: Ping pong images v0.1

--- src/main/kotlin/com/github/sbaldin/tbot/CyberAnny.kt
+++ src/main/kotlin/com/github/sbaldin/tbot/CyberAnny.kt
@@ -3,6 +3,8 @@ package com.github.sbaldin.tbot
 import org.slf4j.LoggerFactory
 import org.telegram.telegrambots.bots.TelegramLongPollingBot
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage
+import org.telegram.telegrambots.meta.api.methods.send.SendPhoto
+import org.telegram.telegrambots.meta.api.objects.InputFile
 import org.telegram.telegrambots.meta.api.objects.Update
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException
 
@@ -32,9 +34,58 @@ class CyberAnnyBot constructor(
      * @param update Содержит сообщение от пользователя.
      */
     override fun onUpdateReceived(update: Update) {
-        val message = update.getMessage().getText()
+        val message = update.message.text
+        val chatId = update.message.chatId.toString()
+
         log.info(message)
-        sendMsg(update.getMessage().getChatId().toString(), message)
+
+        // We check if the update has a message and the message has text
+        if (message.equals("/pic")) {
+            // User sent /pic
+            val msg = SendPhoto().also {
+                it.chatId = chatId
+                it.photo = InputFile("AgACAgQAAxkBAAMfYEmVQCgRgXw_CvssJ9BGk4_JXx4AAnupMRtPI2VT8769TpKTXAXkXiAbAAQBAAMCAAN3AAPuXAYAAR4E")
+                it.caption = "Photo"
+            }
+            execute(msg)
+        } else if (update.hasMessage() && update.message.hasText()) {
+            sendMsg(chatId, message)
+        } else if (update.hasMessage() && update.message.hasPhoto()) {
+            sendPhoto(update, chatId)
+        } else {
+            // Unknown command
+            sendMsg(chatId, "Unknown command")
+        }
+    }
+
+
+    @Synchronized
+    private fun sendPhoto(update: Update, chatId: String) {
+        // Message contains photo
+        // Array with photo objects with different sizes
+        // We will get the biggest photo from that array
+        val photos = update.message.photo
+
+        val biggestPhoto = photos.asSequence().sortedByDescending { it.fileSize }.first()
+        val fileId = biggestPhoto.fileId
+        val width = biggestPhoto.width
+        val height = biggestPhoto.height
+        log.info("File id is $fileId")
+
+        // Set photo caption
+        val caption = """
+                    file_id: $fileId
+                    width: $width
+                    height: $height
+                    """.trimIndent()
+
+        val msg = SendPhoto().apply {
+            photo = InputFile(fileId)
+            setChatId(chatId)
+            setCaption(caption)
+        }
+
+        execute(msg)
     }
 
     /**
